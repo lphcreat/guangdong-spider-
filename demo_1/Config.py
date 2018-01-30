70477af0b8e26239ae70e9fc7c921119df0d9d81
lphcreat <wynfdsm@163.com>
2018-01-30 03:38:23 +0000

    Update Config.py

--- demo_1/Config.py
+++ demo_1/Config.py
@@ -4,4 +4,4 @@ class Config(object):
     SECRET_KEY = 'nfoiqwtcbasdkhsa'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    SQLALCHEMY_DATABASE_URI = "mssql+pyodbc://cfc:!QAZxsw2@218.241.178.211/CrawData?driver=SQL+Server+Native+Client+10.0"
+    SQLALCHEMY_DATABASE_URI = "***"
